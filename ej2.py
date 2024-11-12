9fcbd0fb3374c8aa62cf6813229d0870edbf5452
Markel15 <103565976+Markel15@users.noreply.github.com>
2024-11-12 16:04:56 +0100

    Create ej2.py

---
+++ ej2.py
@@ -0,0 +1,10 @@
+def suma_numeros(a, b):
+    """Función que suma dos números."""
+    return a + b
+
+# Dos variables con números elegidos
+numero1 = 15
+numero2 = 27
+
+# Mostrar la suma usando la función
+print(f"La suma de {numero1} y {numero2} es: {suma_numeros(numero1, numero2)}")
