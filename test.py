b8d53e6a4b6b17a9e9448e118ff4122e44789cd7
Markel15 <103565976+Markel15@users.noreply.github.com>
2024-11-12 16:36:27 +0100

    Añadir prueba +1000

--- test.py
+++ test.py
@@ -14,3 +14,8 @@ def test_setbalance():
     obj = Wallet(0)
     obj.set_balance(40)
     assert obj.get_balance() == 40
+
+def test_incbalance():
+    obj = Wallet(0)
+    obj.inc_mil_balance()
+    assert obj.get_balance() == 1000
